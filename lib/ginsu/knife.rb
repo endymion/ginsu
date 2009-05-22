e4c3b59e67a9283ef263adab37d4a590c386d71f
Ryan Alyn Porter <rap@endymion.com>
2009-05-22 12:48:18 -0400

    Added Ginsu::Knife::initialize.

--- lib/ginsu/knife.rb
+++ lib/ginsu/knife.rb
@@ -1,4 +1,11 @@
 class Ginsu::Knife
+
+  def initialize(params = {})
+    params.each do |key,val|
+      self.send("#{key}=", val) if self.respond_to? key
+    end
+    self.send(:init) if self.respond_to? :init
+  end
   
   def current_status
     @@config.status
