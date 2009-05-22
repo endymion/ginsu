f3572facefb940d944036bb7864cb1b850149f04
Ryan Alyn Porter <rap@endymion.com>
2009-05-22 11:53:29 -0400

    Added configuration.

--- lib/ginsu.rb
+++ lib/ginsu.rb
@@ -1,1 +1,12 @@
-# Ginsu
+module Ginsu; end
+
+require('rubygems')
+
+require('nokogiri')
+
+def require_local(suffix)
+  require(File.expand_path(File.join(File.dirname(__FILE__), suffix)))
+end
+
+require_local('ginsu/config')
+require_local('ginsu/knife')
---
+++ lib/ginsu/config.rb
@@ -0,0 +1,32 @@
+# config.rb contains classes, methods and extends existing Ginsu classes 
+# to provide easy configuration facilities.
+
+module Ginsu
+  # Represents global configuration for Ginsu::Knife.
+  # Can override the following configuration options:
+  # * <tt>source</tt> - the source directory of the static web site.  Defaults to 'static'.
+  class Config
+    include ClassUtilMixin
+    @@ATTRIBUTES = [
+      :source,
+    ]
+    attr_accessor *@@ATTRIBUTES
+    
+  end
+
+  class Knife
+    @@defaults = { 
+      :source => 'site'
+      :slices => []
+    }
+    @@config = Ginsu::Config.new(@@defaults)
+
+    class << self
+      # Yields to given <tt>block</tt> to configure the Ginsu.
+      def configure(&block)
+        raise ArgumentError, "Block must be provided to configure" unless block_given?
+        yield @@config
+      end
+    end
+  end
+end
