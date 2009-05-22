dfd6ba11e9686f1f918733e878f5c8ac59b65e76
Ryan Alyn Porter <rap@endymion.com>
2009-05-22 12:50:29 -0400

    Moved initializer.

--- lib/ginsu/config.rb
+++ lib/ginsu/config.rb
@@ -13,6 +13,13 @@ module Ginsu
     
   end
 
+  def initialize(params = {})
+    params.each do |key,val|
+      self.send("#{key}=", val) if self.respond_to? key
+    end
+    self.send(:init) if self.respond_to? :init
+  end
+
   class Knife
     @@defaults = { 
       :source => 'site',
